2f7e50ed888240a54978d4fa365a8601629c37e4
Julian Mendez <julianlx@hotmail.com>
2022-06-14 18:36:35 -0500

    Hypermedia pagination

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-"""
-function named index_range that takes two integer arguments page and page_size
-"""
+""" Hypermedia Pagination """
 import csv
-import math
-from typing import List
+from math import ceil
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Simple helper function """
+
+    start = (page - 1) * page_size
+    end = page * page_size
+
+    return (start, end)
 
 
 class Server:
@@ -27,29 +34,43 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Use assert to verify that both arguments are integers greater than 0
-        Use index_range to find the correct indexes
-        to paginate the dataset correctly
-        and return the appropriate page of the dataset
-        (i.e. the correct list of rows)
-        """
-        assert (isinstance(page, int) and isinstance(page_size, int)
-                and page > 0 and page_size > 0)
-        range = index_range(page, page_size)
+        """ Finds pagination of dataset """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page > 0
+
         self.dataset()
-        return self.__dataset[range[0]:range[1]]
 
+        if self.__dataset is None:
+            return []
 
-def index_range(page, page_size):
-    """
-    function named index_range that takes two integer arguments:
-    page and page_size.
-    The function should return a tuple of size two containing:
-    a start index and an end index corresponding to
-    the range of indexes to return in a list
-    for those particular pagination parameters.
-    Page numbers are 1-indexed, i.e. the first page is page 1.
-    """
-    previous = (page - 1) * page_size
-    return (previous, previous + page_size)
+        idx_range = index_range(page, page_size)
+        data = self.__dataset[idx_range[0]:idx_range[1]]
+        return data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """ Provides information about dataset """
+        data = self.get_page(page, page_size)
+
+        data_set = self.__dataset
+        len_set = len(data_set) if data_set else 0
+
+        total_pages = ceil(len_set / page_size) if data_set else 0
+
+        if not data:
+            page_size = 0
+        else:
+            page_size = len(data)
+
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+
+        hyper = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
+
+        return hyper
