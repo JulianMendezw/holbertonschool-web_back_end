2c6c268d55f55c595ed90c15d981216faee9bd08
Julian Mendez <julianlx@hotmail.com>
2022-06-25 20:45:22 -0500

    addin auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
-from flask import abort, jsonify, request
-from api.v1.views import app_views
+""" SessionAuth inherits from Auth """
+from api.v1.auth.auth import Auth
 from models.user import User
-import os
+import uuid
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> str:
-    """ POST /api/v1/auth_session/login
-    """
-    email = request.form.get("email")
-    if not email or email == '':
-        return jsonify({"error": "email missing"}), 400
-    password = request.form.get("password")
-    if not password or password == '':
-        return jsonify({"error": "password missing"}), 400
-    objs = User().search({"email": email})
-    if not objs or objs == []:
-        return jsonify({"error": "no user found for this email"}), 404
-    if not objs[0].is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    session_id = auth.create_session(objs[0].id)
-    result = jsonify(objs[0].to_json())
-    result.set_cookie(os.getenv("SESSION_NAME"), session_id)
-    return result
+class SessionAuth(Auth):
+    """ SessionAuth class """
+    user_id_by_session_id = {}
 
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
 
-@app_views.route('/auth_session/logout', methods=['DELETE'],
-                 strict_slashes=False)
-def logout() -> str:
-    """ DELETE /api/v1/auth_session/logout
-    """
-    from api.v1.app import auth
-    if not auth.destroy_session(request):
-        abort(404)
-    return jsonify({}), 200
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ returns a User ID based on a Session ID """
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value: """
+        cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+        user = User.get(user_id)
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ deletes the user session / logout """
+        if not request:
+            return False
+        cookie = self.session_cookie(request)
+        if not cookie:
+            return False
+        if not self.user_id_for_session_id(cookie):
+            return False
+        del self.user_id_by_session_id[cookie]
+        return True
